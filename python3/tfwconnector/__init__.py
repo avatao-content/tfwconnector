6c02204413abc6b86a0c2be932609c72344cb071
Kristóf Tóth <kristof.toth@avatao.com>
2018-05-11 16:26:57 +0200

    Rename files to adhere naming conventions

--- python3/tfwconnector/__init__.py
+++ python3/tfwconnector/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (C) 2018 Avatao.com Innovative Learning Kft.
 # All Rights Reserved. See LICENSE file for details.
 
-from .tfwconnector import TFWServerConnector
+from .tfw_server_connector import TFWServerConnector
